c3d57c8eb0170e6eb6374718c6b998a37fa3acf2
Rahul Kale <rkale7617@gmail.com>
2023-03-14 09:48:39 +0530

    new repo

---
+++ CasePointStudy/Demo/Inheritance.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CasePointStudy.Demo
+{
+    internal class Inheritance
+    {
+    }
+}
---
+++ CasePointStudy/Demo/SwappingWithoutUsingThirdVariable.cs
@@ -0,0 +1,26 @@
+﻿/*using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CasePointStudy.Demo
+{
+    public class SwappingWithoutUsingThirdVariable
+    {
+        static void Main(string[] args)
+        {
+            int a = 5, b = 3, temp;
+
+            //swapping
+            temp = a;
+            a = b;
+            b = temp;
+
+            Console.WriteLine("Values after swapping are:");
+            Console.WriteLine("a=" + a);
+            Console.WriteLine("b=" + b);
+        }
+    }
+}
+*/
