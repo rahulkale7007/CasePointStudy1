01af39a36886be2340aba00f138852eab3440829
Rahul Kale <rkale7617@gmail.com>
2023-03-14 09:55:04 +0530

    new program

---
+++ CasePointStudy/Demo/SwappingUsingThirdVariable.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CasePointStudy.Demo
+{
+    public class SwappingUsingThirdVariable
+    {
+        static void Main(string[] args)
+        {
+            int a = 10, b = 20;
+
+            //swapping
+            a = a + b;
+            b = a - b;
+            a = a - b;
+
+            Console.WriteLine("Values after swapping are:");
+            Console.WriteLine("a=" + a);
+            Console.WriteLine("b=" + b);
+        }
+    }
+}
